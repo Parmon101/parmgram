e1c78b2e85e3fdb3b7f2adec74188d51f19deb10
Evgeny Belyakov <bfjeka101@gmail.com>
2023-04-30 17:31:18 +0300

    Initial

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>1</Text>
+    </SafeAreaView>
+  );
+};
+
+const s = StyleSheet.create({});
+
+export default App;
